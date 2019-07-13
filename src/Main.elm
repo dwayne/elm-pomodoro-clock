f1935af8cc888530c665a7aa2b902ad9b773f4e6
Dwayne Crooks <me@dwaynecrooks.com>
2019-07-13 05:30:17 -0400

    Make settings work
    When you click down the value decreases to as low as 1 and
    when you click up the value increases to as high as 60.

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, a, button, div, footer, h1, h2, h3, i, span, text)
 import Html.Attributes exposing (class, href, target)
+import Html.Events exposing (onClick)
 
 
 main : Program () Model Msg
@@ -36,23 +37,58 @@ init =
 -- UPDATE
 
 
-type alias Msg = Never
+type Msg
+  = ClickedDown Setting
+  | ClickedUp Setting
+
+
+type Setting
+  = Break
+  | Session
 
 
 update : Msg -> Model -> Model
-update _ = identity
+update msg model =
+  case msg of
+    ClickedDown Break ->
+      { model | break = decrement model.break }
+
+    ClickedUp Break ->
+      { model | break = increment model.break }
+
+    ClickedDown Session ->
+      { model | session = decrement model.session }
+
+    ClickedUp Session ->
+      { model | session = increment model.session }
+
+
+increment : Int -> Int
+increment n =
+  if n < 60 then
+    n + 1
+  else
+    n
+
+
+decrement : Int -> Int
+decrement n =
+  if n > 1 then
+    n - 1
+  else
+    n
 
 
 -- VIEW
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view { break, session, timeLeft } =
   div []
     [ h1 [] [ text "Pomodoro Clock" ]
     , div [ class "flex" ]
-        [ viewSetting "Break Length" break
-        , viewSetting "Session Length" session
+        [ viewSetting "Break Length" break Break
+        , viewSetting "Session Length" session Session
         ]
     , viewTimer timeLeft
     , viewControls
@@ -60,15 +96,21 @@ view { break, session, timeLeft } =
     ]
 
 
-viewSetting : String -> Int -> Html msg
-viewSetting title value =
+viewSetting : String -> Int -> Setting -> Html Msg
+viewSetting title value setting =
   div [ class "setting" ]
     [ h2 [] [ text title ]
     , div []
-        [ button [ class "button" ]
+        [ button
+            [ class "button"
+            , onClick (ClickedDown setting)
+            ]
             [ i [ class "fa fa-arrow-down fa-2x" ] [] ]
         , span [] [ text (String.fromInt value) ]
-        , button [ class "button" ]
+        , button
+            [ class "button"
+            , onClick (ClickedUp setting)
+            ]
             [ i [ class "fa fa-arrow-up fa-2x" ] [] ]
         ]
     ]
